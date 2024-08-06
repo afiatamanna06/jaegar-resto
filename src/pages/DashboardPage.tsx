20e68489c9668a37eb93123ef887021bdd4878b5
afiatamanna06 <afiatamanna06@gmail.com>
2024-08-06 21:35:15 +0600

    Add date function

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,38 +1,6 @@
 import React from "react";
 import OrderTable from "../components/dashboard/OrderTable";
-
-export const formatDate = (date: Date): string => {
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-
-  const dayName = days[date.getDay()];
-  const day = date.getDate();
-  const monthName = months[date.getMonth()];
-  const year = date.getFullYear();
-
-  return `${dayName} ${day} ${monthName}, ${year}`;
-};
+import { formatDate } from "../utils/formatDate";
 
 const DashboardPage = () => {
   const date = new Date();
---
+++ src/utils/formatDate.ts
@@ -0,0 +1,32 @@
+export const formatDate = (date: Date): string => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const dayName = days[date.getDay()];
+  const day = date.getDate();
+  const monthName = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${dayName} ${day} ${monthName}, ${year}`;
+};
